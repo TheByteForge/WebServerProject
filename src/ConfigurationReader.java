14d2e4ee562b0ce0a9ef1e8293c93df67fc4a4d1
L-Figgins13 <l.figgins13@gmail.com>
2017-02-04 15:46:58 -0800

    finished ConfigurationReader Super Class

--- src/ConfigurationReader.java
+++ src/ConfigurationReader.java
@@ -5,9 +5,33 @@
 import java.io.*;
 
 
-public class ConfigurationReader {
+public abstract class ConfigurationReader {
 
     private File file;
+    private BufferedReader bufferedReader = null;
 
+    public ConfigurationReader(String fileName)
+    {
+        String path = "~/WebServerProject/config/" + fileName;
+        file = new File(path);
+        bufferedReader = new BufferReader(new FileReader(file));
+    }
+
+    public boolean hasMoreLines()
+    {
+        if(bufferedReader.nextLine() != -1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String nextLine()
+    {
+        return bufferedReader.nextLine();
+    }
+
+
+    public abstract void load();
 
 }
