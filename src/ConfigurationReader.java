39d4b30f54b25bfbf0a497c606703b0ccbf48dcc
L-Figgins13 <l.figgins13@gmail.com>
2017-02-04 14:56:32 -0800

    initial commit

---
+++ src/ConfigurationReader.java
@@ -0,0 +1,13 @@
+/**
+ * Created by euphoric on 2/4/17.
+ */
+
+import java.io.*;
+
+
+public class ConfigurationReader {
+
+    private File file;
+
+
+}
---
+++ src/ResponseFactory.java
@@ -0,0 +1,10 @@
+/**
+ * Created by euphoric on 2/3/17.
+ */
+public class ResponseFactory {
+
+    public Response getResponse(Request request, Resource resource){
+       Response response = new Response(resourse);
+    }
+}
+
