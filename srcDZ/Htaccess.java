94b4f492c0ee42f7fa90811d77839aa9e86b9bed
The Byte Forge <zackwatkins@gmail.com>
2017-02-10 21:52:41 -0800

    Add files via upload

---
+++ srcDZ/Htaccess.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+//public class Htaccess extends ConfigurationReader{
+//	private Htpassword userFile;
+//	private String authType;
+//	private String authName;
+//	private String require;
+//
+//	public Htaccess(String fileName) throws IOException {
+//		super(fileName);
+//	}
+//
+////	public boolean isAuthorized(String username, String password){
+////
+////	}
+//
+//	public void load(){
+//
+//	}
+//}
---
+++ srcDZ/HttpdConf.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class HttpdConf extends ConfigurationReader{
+    private String serverRoot = null;
+    private Integer listen = 0;
+    protected String documentRoot = null;
+    protected String logFile = null;
+	private Dictionary <String, String> alias = new Hashtable();
+    private Dictionary <String, String> scriptAliases = new Hashtable();
+    private String accessFileName = null;
+    private Dictionary <String, String> directoryIndex = new Hashtable<>();
+
+
+    public HttpdConf (){}
+
+	public HttpdConf(String fileName)throws IOException{
+		String confPath = "C:\\Users\\Zack\\Desktop\\DZWS\\conf\\";
+        file = new File(confPath + fileName);
+        FileInputStream inputStream = new FileInputStream(file);
+        br = new BufferedReader(new InputStreamReader(inputStream));
+
+        try{
+
+            while (hasMoreLines()){
+                String tempLine = nextLine();
+                currentLine = tempLine.split("\\s+");
+                if(currentLine[0] != "#" && currentLine[0] != " ") {
+
+                    for (int i = 1; i < currentLine.length; i++) {
+                        switch (currentLine[0]) {
+                            case "ServerRoot":
+                                serverRoot = currentLine[1];
+                                System.out.println(serverRoot);
+                                break;
+                            case "Listen":
+                                listen = Integer.parseInt(currentLine[1]);
+                                System.out.println(listen);
+                                break;
+                            case "DocumentRoot":
+                                documentRoot = currentLine[1];
+                                System.out.println(documentRoot);
+                                break;
+                            case "LogFile":
+                                logFile = currentLine[1];
+                                System.out.println(logFile);
+                                break;
+                            case "Alias":
+                                alias.put(currentLine[1], currentLine[2]);
+                                System.out.println(serverRoot);
+                                break;
+                            case "ScriptAliases":
+                                scriptAliases.put(currentLine[1], currentLine[2]);
+                                System.out.println(serverRoot);
+                                break;
+                            case "AccessFilename":
+                                accessFileName = currentLine[1];
+                                System.out.println(serverRoot);
+                                break;
+                            case "DirectoryIndex":
+                                directoryIndex.put(currentLine[1], currentLine[2]);
+                                System.out.println(serverRoot);
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                }
+
+
+            }
+
+            br.close();
+            if(inputStream!=null){
+                inputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load(){}
+
+
+    public static void main(String [] args){
+        try {
+            HttpdConf hc = new HttpdConf("httpd.conf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+
