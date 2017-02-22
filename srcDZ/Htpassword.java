cbdb4d74da2a007d155831fde06f69fa3d018f9e
Derek Florimonte <dflomo@gmail.com>
2017-02-22 13:41:15 -0800

    Add files via upload

---
+++ srcDZ/Htpassword.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Dictionary;
+
+
+//public class Htpassword extends ConfigurationReader{
+//	private Dictionary users;
+//
+//	public boolean isAuthorized(String username, String password){
+//
+//		return false;
+//	}
+//
+//	public void load(){
+//
+//	}
+//}
